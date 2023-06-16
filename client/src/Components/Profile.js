ffb3645a9b00d1779309ee57850abd19071eb478
Avishmita Shivkumar <avishmita@gmail.com>
2023-06-16 14:45:35 -0400

    Profile component + minor changes to client package & manifest

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,7 +1,40 @@
-const Profile = () =>{
-  return(
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-    <div>profile</div>
+const Profile = () =>{
+  const { profileId } = useParams();
+  const [ profile, setProfile ] = useState();
+
+  useEffect(() => {
+    fetch(`/api/${profileId}/profile`)
+    .then(response => response.json())
+    .then(parsed => {
+      setProfile(parsed.profile);
+    })
+  }, []);
+  console.log(profile);
+
+  return(
+    <> 
+    {!profile ? <h1>Loading...</h1> : 
+    (<> 
+      <div>profile</div>
+      <img src={profile.bannerSrc}/>
+      <img src={profile.avatarSrc}/>
+      <button></button>
+      {/* button "follow/following" - changes based on the profile info. */}
+      <h1>{profile.displayName}</h1>
+      <p>{profile.handle}</p>
+      {/* <p>{profile.isFollowingYou}</p> - do they follow the current user? seems to break everything*/}
+      <p>{profile.bio}</p>
+      <p>{profile.location}</p>
+      <p>{profile.joined}</p>
+      <p>{profile.numFollowers}</p>
+      <p>{profile.numFollowing}</p>
+    </>)
+    }
+    </>
+    
   )
 }
 
