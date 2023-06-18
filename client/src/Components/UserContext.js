35de7deb092daf0f13dff6e8ffc16c5559f44a4e
Gabriel <snow.gabo51@gmail.com>
2023-06-18 11:07:19 -0400

    structure of context

--- client/src/Components/UserContext.js
+++ client/src/Components/UserContext.js
@@ -1,17 +1,31 @@
-import {Children, createContext} from "react"
+import {children, createContext} from "react"
 
 import { useState } from "react"
 
 export const UserContext = createContext(null)
 
-export const UserProvider = useState(()=>{
+export const UserProvider = ({children})=>{
+    
+    const [user, setUser] = useState(null)
+    const [status, setStatus]= useState('loading')
+
+    useEffect(() => {
+        fetch(`/api/me/profile`)
+        .then(response => response.json())
+        .then(parsed => {
+          setUser(parsed.profile);
+        })
+      }, []);
+      console.log(profile);
+
 
 
     return(
-        <UserContext.Provider value={}>
+        <UserContext.Provider value={{user, status}}>
             {children}
         </UserContext.Provider>
     )
 
     
-})
+
+}
