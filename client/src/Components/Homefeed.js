605ae7525f5c56daf3b346fcb929e00731724794
GabGo51 <snow.gabo51@gmail.com>
2023-06-19 12:03:52 -0400

    Created post and modify dates

--- client/src/Components/Homefeed.js
+++ client/src/Components/Homefeed.js
@@ -1,140 +1,153 @@
-
 import { styled } from "styled-components";
 import { UserContext } from "./UserContext";
 import { useContext } from "react";
 import { useEffect, useState } from "react";
 import { COLORS } from "../constant";
-import SmallTweet from "./SmallTweet"
+import SmallTweet from "./SmallTweet";
 
 const Homefeed = () => {
-  const {user, status} = useContext(UserContext)
-  const [number, setNumber] = useState(280)
-  const [tweets, setTweets] = useState()
-  const[value, setValue] = useState("")
- 
-  
- 
-//change on textarea
-  const handleChange = (event) =>{
-    setValue(event.target.value)
-    setNumber(280-value.length)
-    
-  }
+  const { user, status } = useContext(UserContext);
+  const [number, setNumber] = useState(280);
+  const [tweets, setTweets] = useState();
+  const [value, setValue] = useState("");
+  const [feed , setFeed]= useState(true)
+
+  //change on textarea
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    setNumber(280 - value.length);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const postData = {
+      status: value
+    };
+
+    fetch("/api/tweet", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => response.json())
+      .then((newTweet) => {
+        setFeed(!feed)
+        setValue("");
+      })
+      .catch((error) => {
+        console.error("Error submitting tweet:", error);
+      });
+  };
 
   useEffect(() => {
     fetch(`/api/me/home-feed`)
-    .then(response => response.json())
-    .then(parsed => {
-      setTweets(Object.values(parsed.tweetsById));
-    })
-  }, []);
+      .then((response) => response.json())
+      .then((parsed) => {
+        setTweets(Object.values(parsed.tweetsById));
+      });
+  }, [feed]);
   console.log(tweets);
-  
-
 
   return (
     <>
-      {!user ? <h1>Loading...</h1> : 
-      <HomeContainer>
-        <Title>Home</Title>
-        <TweetBox>
-          <ProfileImage src={user.avatarSrc}/>
-          <Input 
-          placeholder="What's happening?"
-          type="text"
-          value={value}
-          onChange={handleChange}
-          />
-          <PostingSection>
-            <Number number={number}>{number}</Number>
-            <Button>Meow</Button>
-          </PostingSection>
-        </TweetBox>
-        
-        {!tweets ?<h1>Loading...</h1>:
-        tweets.map(tweet=>{
-          return(
-            <SmallTweet key = {tweet.id} tweet = {tweet}/>
-          )
-        })
-        }
+      {!user ? (
+        <h1>Loading...</h1>
+      ) : (
+        <HomeContainer>
+          <Title>Home</Title>
+          <TweetBox>
+            <ProfileImage src={user.avatarSrc} />
+            <Input
+              placeholder="What's happening?"
+              type="text"
+              value={value}
+              onChange={handleChange}
+            />
+            <PostingSection>
+              <Number number={number}>{number}</Number>
+              <Button type="submit" onClick={handleSubmit}>
+                Meow
+              </Button>
+            </PostingSection>
+          </TweetBox>
 
-      </HomeContainer>}
+          {!tweets ? (
+            <h1>Loading...</h1>
+          ) : (
+            tweets.slice().reverse().map((tweet) => {
+              return <SmallTweet key={tweet.id} tweet={tweet} />;
+            })
+          )}
+        </HomeContainer>
+      )}
     </>
-    
-    
   );
 };
 
-
-
 const HomeContainer = styled.div`
-width: calc(100vw - 25rem);
+  width: calc(100vw - 25rem);
 
-@media screen and (max-width: 53rem){
-    
-  width: calc(100vw - 12.5rem);
-}
-display:flex;
-flex-direction:column;
-
-`
+  @media screen and (max-width: 53rem) {
+    width: calc(100vw - 12.5rem);
+  }
+  display: flex;
+  flex-direction: column;
+`;
 const Title = styled.div`
+  font-weight: bold;
+  font-size: 1.5em;
+  padding: 1rem;
 
-font-weight: bold;
-font-size: 1.5em;
-padding: 1rem;
-
-border: 1px solid ${COLORS.paleGrey};
-
-`
+  border: 1px solid ${COLORS.paleGrey};
+`;
 
 const TweetBox = styled.div`
-position: relative;
-border: 1px solid ${COLORS.paleGrey};
-`
+  position: relative;
+  border: 1px solid ${COLORS.paleGrey};
+`;
 
 const Input = styled.textarea`
-width: 100%;
-position: relative;
-border:none;
-outline: none;
-padding-bottom: 12.5rem;
-padding-top: 1.8rem;
-padding-left:5.8rem;
-padding-right: 0.7rem;
-font-size: 1.2em;
-resize: none;
-overflow: hidden;
-`
+  width: 100%;
+  position: relative;
+  border: none;
+  outline: none;
+  padding-bottom: 12.5rem;
+  padding-top: 1.8rem;
+  padding-left: 5.8rem;
+  padding-right: 0.7rem;
+  font-size: 1.2em;
+  resize: none;
+  overflow: hidden;
+`;
 
 const ProfileImage = styled.img`
-position: absolute;
-top: 1rem;
-left: 1rem;
-width: 4rem;
-border-radius: 50%;
-z-index: 2;
-`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  width: 4rem;
+  border-radius: 50%;
+  z-index: 2;
+`;
 
-const PostingSection = styled.div`
-position: absolute;
-top: 13rem;
-right: .7rem;
-display: flex;
-align-items: center;
-justify-content: center;
-`
+const PostingSection = styled.form`
+  position: absolute;
+  top: 13rem;
+  right: 0.7rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Number = styled.p`
-margin-right: .7rem;
-margin-bottom: -2rem;
-color: ${props => props.number < 0 ? "red" : "#21B474"};
-`
+  margin-right: 0.7rem;
+  margin-bottom: -2rem;
+  color: ${(props) => (props.number < 0 ? "red" : "#21B474")};
+`;
 const Button = styled.button`
-
-  margin-top: .7rem;
-  padding: .9rem 1.1rem;
+  margin-top: 0.7rem;
+  padding: 0.9rem 1.1rem;
   border-radius: 2.5rem;
   border: none;
   border: 1px solid hsl(258deg, 100%, 50%);
