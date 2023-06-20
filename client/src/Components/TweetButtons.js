f0b26ae93d8a7f0473dfa58389f0c9075b82e1bc
GabGo51 <snow.gabo51@gmail.com>
2023-06-19 20:22:47 -0400

    It works but it not a put hihi

--- client/src/Components/TweetButtons.js
+++ client/src/Components/TweetButtons.js
@@ -1,48 +1,55 @@
-import { styled } from "styled-components"
-import { COLORS } from "../constant"
-import { useState } from "react"
-const TweetButton = () =>{
+import { styled } from "styled-components";
+import { COLORS } from "../constant";
+import { useState } from "react";
+const TweetButton = ({tweet}) => {
+  const [clicked, setClicked] = useState(false);
 
-  const [clicked, setClicked] = useState(false)
-
-  const handleClick = () =>{
-    setClicked(!clicked)
+  const handleClick = () => {
+    setClicked(!clicked);
     //do a patch
-  }
+  };
+  console.log(tweet);
 
-return(
+  return (
+    <Container>
+      <i className="fa-regular fa-comment"></i>
+      <i className="fa-sharp fa-solid fa-retweet"></i>
+      <Liked>
+        <i
+          className={`fa-regular fa-heart ${clicked ? "liked" : ""}`}
+          onClick={handleClick}
+        ></i>
+        {clicked && <span className="count">1</span>}
+      </Liked>
 
-  <Container>
-    <i className="fa-regular fa-comment"></i>
-    <i className="fa-sharp fa-solid fa-retweet"></i>
-    <i
-        className={`fa-regular fa-heart ${clicked ? "liked" : ""}`}
-        onClick={handleClick}
-      ></i>
-    <i className="fa-sharp fa-solid fa-arrow-up-from-bracket"></i>
-  </Container>
-)
-}
+      <i className="fa-sharp fa-solid fa-arrow-up-from-bracket"></i>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-height: 3rem;
-border: 1px solid ${COLORS.paleGrey};
-border-top:none;
-border-bottom: none;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 3rem;
+  border: 1px solid ${COLORS.paleGrey};
+  border-top: none;
+  border-bottom: none;
 
-
-i{
-  cursor: pointer;
-  transition: 200ms;
-}
-
-.liked {
-    color: red;
-    
+  i {
+    cursor: pointer;
+    transition: 200ms;
   }
+
+  .liked {
+    color: red;
+  }
+`;
+
+const Liked = styled.div`
+display: flex;
+justify-content: flex-end;
+align-items: center;
 `
 
-export default TweetButton
+export default TweetButton;
