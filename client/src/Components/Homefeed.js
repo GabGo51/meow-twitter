217294f47c087f6201a299c7bb0d44130ac79d73
GabGo51 <snow.gabo51@gmail.com>
2023-06-20 10:34:02 -0400

    .

--- client/src/Components/Homefeed.js
+++ client/src/Components/Homefeed.js
@@ -38,6 +38,7 @@ const Homefeed = () => {
           setFeed(!feed)
           setValue("");
         }else {
+          event.preventDefault()
           window.alert("Tweet to long!")
         }
         
