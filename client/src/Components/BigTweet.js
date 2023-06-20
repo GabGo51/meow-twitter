372401b9672370274dfbbbc1f6c0ac1936fb153d
GabGo51 <snow.gabo51@gmail.com>
2023-06-19 23:05:41 -0400

    get specific tweet with tweet details

--- client/src/Components/BigTweet.js
+++ client/src/Components/BigTweet.js
@@ -1,5 +1,88 @@
-const BigTweet = () =>{
+import { styled } from "styled-components";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import { COLORS } from "../constant";
 
-}
+const BigTweet = ({ tweet }) => {
+  return (
+    <TweetContainer>
+      <Link to={`/${tweet.author.handle}/profile`}>
+        <ImageProfile alt="profile picture" src={tweet.author.avatarSrc} />
+      </Link>
+      <TweetInfo>
+        <Title>
+          <Name to={`/${tweet.author.handle}/profile`}>
+            {tweet.author.displayName}
+          </Name>{" "}
+          @{tweet.author.handle} · {moment(tweet.timestamp).format("MMM, Do")}
+        </Title>
+        <Box >
+          <Text>{tweet.status}</Text>
+          <ImageBox>
+            {tweet.media && tweet.media.length > 0 && (
+              <Image src={tweet.media[0].url} alt="tweet image" />
+            )}
+          </ImageBox>
+        </Box>
+      </TweetInfo>
+    </TweetContainer>
+  );
+};
 
-export default BigTweet
+const TweetContainer = styled.div`
+display:flex;
+padding-top:1rem;
+padding-left:1rem;
+border: 1px solid ${COLORS.paleGrey};
+width: calc(100vw - 25rem);
+height: 100%;
+padding-top: 20px;
+
+`
+
+const ImageProfile = styled.img`
+width:4rem;
+height:4rem;
+border-radius:50%;
+margin-right:1rem;
+margin-bottom: 1rem;
+
+`
+const TweetInfo = styled.div`
+display:flex;
+flex-direction:column;
+flex-wrap: wrap;
+`
+
+const Box = styled.div`
+
+`
+
+const Name = styled(Link)`
+font-weight:bold;
+color:black;
+margin-right:1rem;
+text-decoration: none;
+`
+const Title = styled.p`
+color:grey;
+display:flex;
+`
+
+const Text = styled.p`
+margin-bottom:1rem;
+
+
+`
+
+const ImageBox = styled.div`
+
+`
+
+const Image = styled.img`
+border-radius:10px;
+width:98%;
+object-fit:cover;
+`
+
+export default BigTweet;
--- client/src/Components/TweetDetails.js
+++ client/src/Components/TweetDetails.js
@@ -1,12 +1,29 @@
 import { useParams } from "react-router-dom";
-
+import BigTweet from "./BigTweet";
+import { useEffect, useState } from "react";
 const TweetDetails = () =>{
   const { tweetId } = useParams();
+  const [tweet, setTweet] = useState("")
   console.log(tweetId);
+
+  useEffect(() => {
+    fetch(`/api/tweet/${tweetId}`)
+    .then(response => response.json())
+    .then(parsed => {
+      setTweet(parsed.tweet) 
+    })
+  }, []);
+  console.log(tweet);
+  
+
+
   
   return(
 
-    <div>TweetDetails</div>
+    <>{!tweet ? (
+      <h1>Loading...</h1>
+    ) : (<BigTweet tweet = {tweet}/>)}
+    </>
   )
 }
 
